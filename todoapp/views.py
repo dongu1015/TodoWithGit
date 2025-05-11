a864468954ac499ca0e9454eded7ecbd9bb5d2ba
yongB2 <rlarlfyd0601@naver.com>
2025-05-11 20:41:38 +0900

    test

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+import subprocess
+from django.http import JsonResponse
+import json
 
-# Create your views here.
+def commit_view(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        msg = data.get('message', '기본 메시지')
+
+        try:
+            subprocess.run(['git', 'add', '.'])
+            subprocess.run(['git', 'commit', '-m', msg])
+            return JsonResponse({'result': 'success'})
+        except Exception as e:
+            return JsonResponse({'result': 'error', 'detail': str(e)})
